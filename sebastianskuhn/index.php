8042856c93c6fdf318c40ab95d83141557648285
Sebastian <sebastianskuhn@gmail.com>
2024-11-19 10:09:19 +1100

    testing

--- sebastianskuhn/index.php
+++ sebastianskuhn/index.php
@@ -3,6 +3,6 @@ $title = "Main Page";
 include("includes/header.inc");?>
 <body>
     <h1>Page Under Construction</h1>
-    <a href="projects.php">Link </a>
+    <a href="projects/projects.php">Link</a>
 </body>
 </html>
